35e59992d226ed44a8e8bfca3ae0131924555d83
Boris <boris.eder@gmail.com>
2025-08-18 21:43:14 +0200

    Initial Commit

---
+++ StoryTeller2/StoryTeller2App.swift
@@ -0,0 +1,32 @@
+//
+//  StoryTeller2App.swift
+//  StoryTeller2
+//
+//  Created by Boris Eder on 18.08.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StoryTeller2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ StoryTeller2Tests/StoryTeller2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  StoryTeller2Tests.swift
+//  StoryTeller2Tests
+//
+//  Created by Boris Eder on 18.08.25.
+//
+
+import Testing
+
+struct StoryTeller2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
